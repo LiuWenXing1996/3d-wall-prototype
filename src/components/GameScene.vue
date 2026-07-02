614cd54218e1a6b93d52095a8d6d32b2f9bc0f21
柳问星 <lwx_redstone@163.com>
2026-07-02 08:08:38 +0800

    feat: 将一些测试按钮转移到gui中

--- src/components/GameScene.vue
+++ src/components/GameScene.vue
@@ -1,52 +1,11 @@
 <template>
     <canvas class="game-canvas" ref="canvas"></canvas>
     <!-- <div ref="container"></div> -->
-    <div class="ui-controller">
-        <div>游戏控制</div>
-        <button @click="game?.start">开始游戏</button>
-        <button @click="game?.pause">暂停游戏</button>
-        <button @click="game?.resume">继续游戏</button>
-        <div>产生新连块</div>
-        <button @click="game?.gameBoard?.spawnPolyominoByType('I')">产生I连块</button>
-        <button @click="game?.gameBoard?.spawnPolyominoByType('J')">产生J连块</button>
-        <button @click="game?.gameBoard?.spawnPolyominoByType('L')">产生L连块</button>
-        <button @click="game?.gameBoard?.spawnPolyominoByType('O')">产生O连块</button>
-        <button @click="game?.gameBoard?.spawnPolyominoByType('S')">产生S连块</button>
-        <button @click="game?.gameBoard?.spawnPolyominoByType('T')">产生T连块</button>
-        <button @click="game?.gameBoard?.spawnPolyominoByType('Z')">产生Z连块</button>
-        <div>方块控制</div>
-        <div>
-            <button @click="game?.gameBoard?.stepMoveCurrentPolyominoZ(1)">↙️左下移</button>
-            <button @click="game?.gameBoard?.stepMoveCurrentPolyominoZ(-1)">↗️右上移</button>
-            <button @click="game?.gameBoard?.stepMoveCurrentPolyominoX(-1)">↖️左上移</button>
-            <button @click="game?.gameBoard?.stepMoveCurrentPolyominoX(1)">↘️右下移</button>
-            <button @click="game?.gameBoard?.tryDrop()">⬇️下移</button>
-            <button @click="game?.gameBoard?.stepMoveCurrentPolyominoY(1)">⬆️上移</button>
-        </div>
-        <div>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoX(1)">x轴右旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoPreview(1)">x轴预览右旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoX(-1)">x轴左旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoPreview(-1)">x轴预览左旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoY(1)">y轴右旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoPreview(0, 1)">y轴预览右旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoY(-1)">y轴左旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoPreview(0, -1)">y轴预览左旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoZ(1)">z轴右旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoPreview(0, 0, 1)">z轴预览右旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoZ(-1)">z轴左旋</button>
-            <button @click="game?.gameBoard?.stepRotateCurrentPolyominoPreview(0, 0, -1)">z轴预览左旋</button>
-        </div>
-        <div>整体控制</div>
-        <button @click="game?.gameBoard?.stepRotateOnCenter(1)">左转</button>
-        <button @click="game?.gameBoard?.stepRotateOnCenter(-1)">右转</button>
-    </div>
 </template>
 <script setup lang="ts">
 import { onMounted, ref, shallowRef } from 'vue';
 import Game from '../three/Game';
 import { initGui } from '../three/gui';
-import rotateImg from '../assets/竖直-右旋.svg'
 const canvas = ref<HTMLCanvasElement | null>(null);
 const game = shallowRef<Game | null>(null);
 
@@ -62,12 +21,5 @@ onMounted(() => {
     width: 100%;
     height: 100%;
     display: block;
-}
-
-.ui-controller {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    background-color: rgba(255, 255, 255, 0.8);
 }
 </style>
--- src/three/gui.ts
+++ src/three/gui.ts
@@ -4,15 +4,19 @@ import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { GridHelper } from "three";
 import type { Nullable } from "../types";
 import CustomAxesHelper from "./CustomAxesHelper";
-import Polyomino from "./Polyomino";
-const gui = new GUI();
-
+import Polyomino, { PolyominoTypeEnum } from "./Polyomino";
+const gui = new GUI({
+  title: "开发测试",
+  // closeFolders: true,
+});
+gui.domElement.style.height = "500px";
+// TODO:将一些测试按钮添加到gui中
 /**
  * 添加开发用的gui
  * @param game 游戏实例
  */
-export const addDevGui = (game: Game) => {
-  const folder = gui.addFolder("开发");
+export const addSceneGui = (game: Game) => {
+  const folder = gui.addFolder("场景管理");
   // 支持自由视角切换
   {
     const controls = new OrbitControls(game.camera, game.renderer.domElement);
@@ -53,92 +57,185 @@ export const addDevGui = (game: Game) => {
       });
   }
 };
-export const addStraightTetrominoGui = (game: Game) => {
-  const folder = gui.addFolder("直线连块");
-  let testTetromino: Nullable<Polyomino> = null;
-  // 添加一个测试直线连块
+export const addGameGui = (game: Game) => {
+  const folder = gui.addFolder("游戏控制");
+  // 开始游戏
   {
     folder
       .add(
         {
-          addTetromino: () => {
-            if (!testTetromino) {
-              // testTetromino = new Polyomino();
-              // testTetromino.position.set(5, 20, 5);
-              // game.gameBoard.add(testTetromino);
-            } else {
-            }
+          start: () => {
+            game.start();
           },
         },
-        "addTetromino",
+        "start",
       )
-      .name("添加测试连块");
+      .name("开始游戏");
   }
-  // 移除测试连块
+  // 暂停游戏
   {
     folder
       .add(
         {
-          removeTestTetromino: () => {
-            if (testTetromino) {
-              game.gameBoard.remove(testTetromino);
-              testTetromino = null;
-            }
+          pause: () => {
+            game.pause();
           },
         },
-        "removeTestTetromino",
+        "pause",
       )
-      .name("移除测试连块");
+      .name("暂停游戏");
   }
-
-  // 沿y轴旋转测试连块
+  // 继续游戏
   {
     folder
       .add(
         {
-          rotateY: () => {
-            if (testTetromino) {
-              testTetromino.stepRotateY(1);
-            }
+          resume: () => {
+            game.resume();
           },
         },
-        "rotateY",
+        "resume",
       )
-      .name("沿y轴旋转测试连块");
+      .name("继续游戏");
   }
-  // 沿z轴旋转测试连块
+};
+export const addBoardGui = (game: Game) => {
+  const folder = gui.addFolder("棋盘调试");
+  // 左转
   {
     folder
       .add(
         {
-          rotateZ: () => {
-            if (testTetromino) {
-              testTetromino.stepRotateZ(1);
-            }
+          rotateLeft: () => {
+            game.gameBoard.stepRotateOnCenter(-1);
           },
         },
-        "rotateZ",
+        "rotateLeft",
       )
-      .name("沿z轴旋转测试连块");
+      .name("左转");
   }
-  // 打印测试连块位置
+  // 右转
   {
     folder
       .add(
         {
-          logPosition: () => {
-            if (testTetromino) {
-              testTetromino.logPosition();
-            }
+          rotateRight: () => {
+            game.gameBoard.stepRotateOnCenter(1);
           },
         },
-        "logPosition",
+        "rotateRight",
       )
-      .name("打印测试连块位置");
+      .name("右转");
+  }
+};
+export const addPolyominoGui = (game: Game) => {
+  const folder = gui.addFolder("连块调试");
+  // 添加连块
+  {
+    const addPolyominoFolder = folder.addFolder("添加连块");
+    addPolyominoFolder
+      .add(
+        {
+          spawnPolyomino: () => {
+            game.gameBoard.spawnPolyomino();
+          },
+        },
+        "spawnPolyomino",
+      )
+      .name("随机添加连块");
+    Object.values(PolyominoTypeEnum).map((type) => {
+      addPolyominoFolder
+        .add(
+          {
+            spawnPolyominoByType: () => {
+              game.gameBoard.spawnPolyominoByType(type);
+            },
+          },
+          "spawnPolyominoByType",
+        )
+        .name(`添加${type}型连块`);
+    });
+  }
+  // 连块移动控制
+  {
+    const movePolyominoFolder = folder.addFolder("连块移动控制");
+    ["X", "Y", "Z"].map((axis) => {
+      [1, -1].map((step) => {
+        movePolyominoFolder
+          .add(
+            {
+              move: () => {
+                if (axis === "X") {
+                  game.gameBoard.stepMoveCurrentPolyominoX(step);
+                } else if (axis === "Y") {
+                  game.gameBoard.stepMoveCurrentPolyominoY(step);
+                } else if (axis === "Z") {
+                  game.gameBoard.stepMoveCurrentPolyominoZ(step);
+                }
+              },
+            },
+            `move`,
+            step,
+          )
+          .name(`向${axis}轴${step > 0 ? "正向" : "负向"}移动`);
+      });
+    });
+  }
+  // 连块旋转控制
+  {
+    const rotatePolyominoFolder = folder.addFolder("连块旋转控制");
+    ["X", "Y", "Z"].map((axis) => {
+      [1, -1].map((step) => {
+        rotatePolyominoFolder
+          .add(
+            {
+              rotate: () => {
+                if (axis === "X") {
+                  game.gameBoard.stepRotateCurrentPolyominoX(step);
+                } else if (axis === "Y") {
+                  game.gameBoard.stepRotateCurrentPolyominoY(step);
+                } else if (axis === "Z") {
+                  game.gameBoard.stepRotateCurrentPolyominoZ(step);
+                }
+              },
+            },
+            `rotate`,
+            step,
+          )
+          .name(`以${axis}轴${step > 0 ? "顺时针" : "逆时针"}旋转`);
+      });
+    });
+  }
+  // 连块旋转预览
+  {
+    const previewPolyominoFolder = folder.addFolder("连块旋转预览");
+    ["X", "Y", "Z"].map((axis) => {
+      [1, -1].map((step) => {
+        previewPolyominoFolder
+          .add(
+            {
+              preview: () => {
+                if (axis === "X") {
+                  game.gameBoard.stepRotateCurrentPolyominoPreview(step, 0, 0);
+                } else if (axis === "Y") {
+                  game.gameBoard.stepRotateCurrentPolyominoPreview(0, step, 0);
+                } else if (axis === "Z") {
+                  game.gameBoard.stepRotateCurrentPolyominoPreview(0, 0, step);
+                }
+              },
+            },
+            `preview`,
+            step,
+          )
+          .name(`预览${axis}轴${step > 0 ? "顺时针" : "逆时针"}旋转`);
+      });
+    });
   }
 };
 
 export const initGui = (game: Game) => {
-  addDevGui(game);
-  addStraightTetrominoGui(game);
+  addSceneGui(game);
+  addGameGui(game);
+  addBoardGui(game);
+  addPolyominoGui(game);
 };
